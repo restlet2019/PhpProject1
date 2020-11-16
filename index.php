5d1bae6adf50a82accb794a9b8782654ac644b9b
Rafael G <rafoga87@gmail.com>
2020-11-16 17:05:37 -0500

    V1

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+
+        <h1>Saludos</h1>
+        <script type="text/javascript">
+for (var i = 0; i < 3; i++) {
+
+  setTimeout(function() { alert(i); }, 1000 + i);
+
+}
+
+        </script>
+    </body>
+</html>
